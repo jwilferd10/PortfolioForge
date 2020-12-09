069e6cf084cc15b388850117f98771cef4904948
Jonathan Wilferd <jwilferd10@yahoo.com>
2020-12-09 13:55:36 -0800

    Feat: Introduction to node.js

---
+++ app.js
@@ -0,0 +1,16 @@
+const profileDataArgs = process.argv.slice(2);
+
+const printProfileData = profileDataArr => {
+  // This...
+  for (let i = 0; i < profileDataArr.length; i += 1) {
+    console.log(profileDataArr[i]);
+  }
+
+  console.log('================');
+
+  // Is the same as this...
+  profileDataArr.forEach(profileItem => console.log(profileItem));
+};
+
+printProfileData(profileDataArgs);
+
