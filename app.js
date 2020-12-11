4fd06665c4029bae3dcdf76fd9e9cbb7c224e43e
Jonathan Wilferd <jwilferd10@yahoo.com>
2020-12-10 17:23:40 -0800

    Feat: Moved generate page to page-template

--- app.js
+++ app.js
@@ -1,16 +1,14 @@
-const profileDataArgs = process.argv.slice(2);
+const fs = require('fs');
 
-const printProfileData = profileDataArr => {
-  // This...
-  for (let i = 0; i < profileDataArr.length; i += 1) {
-    console.log(profileDataArr[i]);
-  }
+const generatePage = require('./src/page-template.js');
 
-  console.log('================');
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+// Works fine: const profileDataArgs = process.argv.slice(2);
 
-  // Is the same as this...
-  profileDataArr.forEach(profileItem => console.log(profileItem));
-};
+const [name, github] = profileDataArgs;
 
-printProfileData(profileDataArgs);
+fs.writeFile('./index.html', generatePage(name, github), err => {
+  if (err) throw new Error(err);
 
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
---
+++ src/page-template.js
@@ -0,0 +1,20 @@
+const generatePage = (name, github) => {
+    return `
+    <!DOCTYPE html> 
+    <html lang="en"> 
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <title>Portfolio Demo</title>
+    </head>
+  
+    <body>
+      <h1>${name}</h1>
+      <h2><a href="https://github.com/${github}">Github</a></h2>
+    </body>
+    </html>
+    `;
+  };
+
+  module.exports = generatePage;
