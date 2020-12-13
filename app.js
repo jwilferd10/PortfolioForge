8bc4b95040a4b8e4efbae731fc099a0f2f36d7b4
Jonathan Wilferd <jwilferd10@yahoo.com>
2020-12-12 19:12:46 -0800

    Feat: Includes inquirer now

--- app.js
+++ app.js
@@ -1,14 +1,40 @@
-const fs = require('fs');
+const inquirer = require('inquirer')
 
-const generatePage = require('./src/page-template.js');
+inquirer
+  .prompt ([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
 
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-// Works fine: const profileDataArgs = process.argv.slice(2);
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
 
-const [name, github] = profileDataArgs;
+// const pageHTML = generatePage(name, github);
 
-fs.writeFile('./index.html', generatePage(name, github), err => {
-  if (err) throw new Error(err);
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if (err) throw err;
 
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
+
+
+
+// COMMENTED OUT 
+
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
+
+// const profileDataArgs = process.argv.slice(2, process.argv.length);
+// // Works fine: const profileDataArgs = process.argv.slice(2);
+
+// const [name, github] = profileDataArgs;
+
+// fs.writeFile('./index.html', generatePage(name, github), err => {
+//   if (err) throw new Error(err);
+
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
