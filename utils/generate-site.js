a6f94a911e62e3672cbb7536b35eff28b7afe141
Jonathan Wilferd <jwilferd10@yahoo.com>
2023-09-06 07:36:08 -0700

    Refactored generate-site.js to use modern imports, exporting each function individually now also. Helpful comments added

--- utils/generate-site.js
+++ utils/generate-site.js
@@ -1,13 +1,15 @@
-const fs = require('fs');
+import fs from 'fs';
 
-const writeFile = fileContent => {
+export const writeFile = fileContent => {
     return new Promise((resolve, reject) => {
         fs.writeFile('./dist/index.html', fileContent, err => {
+            // If err is true, reject promise
             if (err) {
                 reject(err);
                 return;
             }
 
+            // Resolve the promise and send successful data
             resolve({
                 ok: true,
                 message: 'File created!'
@@ -16,14 +18,16 @@ const writeFile = fileContent => {
     });
 };
 
-const copyFile = () => {
+export const copyFile = () => {
     return new Promise((resolve, reject) => {
         fs.copyFile('./src/style.css', './dist/style.css', err => {
+            // If err is true, reject promise
             if (err) {
                 reject(err);
                 return;
             }
 
+            // Resolve the promise and copy the stylesheet
             resolve({
                 ok: true,
                 message: 'Stylesheet Created!'
@@ -31,5 +35,3 @@ const copyFile = () => {
         });
     });
 };
-
-module.exports = { writeFile, copyFile };
