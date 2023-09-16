4b0116766e1985a64fe43cabf75d8f714a1acb1b
Jonathan Wilferd <jwilferd10@yahoo.com>
2023-09-15 20:47:03 -0700

    Created a new file called clearDistDirectory.js, this will handle the process of removing items within the dist folder.

---
+++ utils/clearDistDirectory.js
@@ -0,0 +1,11 @@
+// import fs
+
+// Remove the files inside /dist directory
+
+    // define dist
+
+    // try to get a list of all files
+    
+        // loop through the list and delete each ile
+
+    // log results
