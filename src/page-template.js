2fec4e95780acdb53e4636c83c80093c3a4f24c7
Jonathan Wilferd <jwilferd10@yahoo.com>
2023-09-17 22:10:35 -0700

    Added rounded-edges and box-shadow to work card

--- src/page-template.js
+++ src/page-template.js
@@ -121,7 +121,7 @@ const generateProjects = (projectsArr, emojis) => {
       <div class="flex-row justify-space-between">
       ${projectsArr.filter(({ feature }) => feature).map(({ name, description, languages, link }) => {
         return `
-          <div class="col-12 mb-2 bg-dark text-light p-3">
+          <div class="col-12 mb-2 bg-dark text-light p-3 rounded-edges2 box-shadow2">
             <h3 class="portfolio-item-title text-light">${name}</h3>
             <h5 class="portfolio-languages">
               Built With:
