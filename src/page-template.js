f726c7a7c1a2522949d4bdc0a8f9082844025a07
Jonathan Wilferd <jwilferd10@yahoo.com>
2023-09-19 07:48:25 -0700

    header.name applied to page-template's html title

--- src/page-template.js
+++ src/page-template.js
@@ -197,7 +197,7 @@ export const generatePage = (templateData) => {
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <meta http-equiv="X-UA-Compatible" content="ie=edge">
-      <title>Portfolio Demo</title>
+      <title>${header.name}</title>
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
       <link href="https://fonts.googleapis.com/css?family=Public+Sans:300i,300,500&display=swap" rel="stylesheet">
       <link rel="stylesheet" href="style.css">
