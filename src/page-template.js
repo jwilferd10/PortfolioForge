84814a0703b2d88511fbf6722e2d8fc4357a13c7
Jonathan Wilferd <jwilferd10@yahoo.com>
2023-09-22 18:53:35 -0700

    Refactored page-template on generateSocialMediaLinksHTML(), removed extra toLowerCase() call.

--- src/page-template.js
+++ src/page-template.js
@@ -185,7 +185,7 @@ const generataSocialMediaLinksHTML = (socialMediaData) => {
     
     return `
       <a class="socialLinks ml-2 my-1 px-2 py-1 bg-secondary text-dark d-flex align-items-center" href="${url}">
-        <i class="socialIcon fab fa-${platform.toLowerCase()} fa-2x"></i>
+        <i class="socialIcon fab fa-${platform} fa-2x"></i>
       </a>
     `;
   }).join('');
