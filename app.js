42a79df9a6fa68ca5d822db7f4eefa5c4b15c996
Jonathan Wilferd <jwilferd10@yahoo.com>
2023-09-25 08:02:31 -0700

    Refactored app.js further, moving away from the promise chain using the async/await structure to maintain the same logic.

--- app.js
+++ app.js
@@ -1,51 +1,47 @@
-// Importing necessary modules
 import inquirer from 'inquirer';
+
+// utils imports
 import { writeFile, copyGenericCSSFile } from './utils/generate-site.js';
-import { generatePage } from './src/page-template.js';
 import { clearDistDirectory } from './utils/clearDistDirectory.js';
+
+// src imports
+import { generatePage } from './src/page-template.js';
 import { promptEducation } from './src/promptEducation.js';
 import { promptProject } from './src/promptProject.js';
 import { promptSocialMedia } from './src/promptSocialMedia.js';
 import { promptUser } from './src/promptUser.js';
 
-clearDistDirectory();
-promptUser()
-  .then(userResponses => {
-    // Check if the user is including Social Media links
-    if (userResponses.confirmSocialMedia) {
-        return promptSocialMedia(userResponses);
-    };
+// Main execution of project
+const main = async () => {
+  try {
+    // Clear /dist directory when user initializes the app
+    clearDistDirectory();
 
-    // Check if the user is including Education/Certifications
-    if (userResponses.confirmEducation) {
-      // Call promptEducation and pass portfolioData
-      return promptEducation(userResponses).then(educationData => ({
-        ...userResponses,
-        educationData
-      }));
+    // Await for promptUser, the module's data will be userResponses
+    const userResponses = await promptUser();
+
+    // Run promptSocialMedia if confirmed
+    if (userResponses.confirmSocialMedia) {
+      await promptSocialMedia(userResponses);
     }
-    return userResponses;
-  })
-  // Prompt for project information
-  .then(promptProject)
-  // Generate the HTML page based on provided user data
-  .then(portfolioData => {
-    return generatePage(portfolioData);
-  })
-  // Write generated HTML onto file
-  .then(pageHTML => {
-    return writeFile(pageHTML);
-  })
-  // Copy CSS File and return it
-  .then(writeFileResponse => {
-    console.log(writeFileResponse);
-    return copyGenericCSSFile();
-  })
-  // Log success message after copying files
-  .then(copyFileResponse => {
-    console.log(copyFileResponse);
-  })
-  // Handle any errors that occur during the process
-  .catch(err => {
-    console.log(err);
-  });
+
+    // Run promptEducation if confirmed
+    if (userResponses.confirmEducation) {
+      await promptEducation(userResponses);
+    }
+
+    // Run promptProject and colleect module's data as portfolio data, pass onto pageHTML
+    const portfolioData = await promptProject(userResponses);
+    const pageHTML = generatePage(portfolioData)
+
+    // Generate the appropriate files
+    await writeFile(pageHTML);
+    await copyGenericCSSFile();
+
+    console.log('Portfolio generated successfully.');
+  } catch (error) {
+    console.error('An error has occurred:', error.message);
+  }
+};
+
+main();
