7f74b1bbdd145112d103e7b806127828bcf2a8c8
Jonathan Wilferd <jwilferd10@yahoo.com>
2023-09-26 16:01:32 -0700

    Removed redundant comment. Also fixed spacing issue within promptProject

--- src/promptProject.js
+++ src/promptProject.js
@@ -3,75 +3,66 @@ import inquirer from 'inquirer';
 
 // Function to prompt the user for project information
 export const promptProject = async (portfolioData) => {
-    if (!portfolioData.projects) {
-      portfolioData.projects = [];
-    } 
+  if (!portfolioData.projects) {
+    portfolioData.projects = [];
+  } 
   
-    console.log(`
-      =================
-      Add a New Project
-      =================
-    `);
+  console.log(`
+    =================
+    Add a New Project
+    =================
+  `);
   
-    // Project Name, Descrpition, Structural Languages, Link, Feature
-    const projectQuestions = [
-      {
-        type: 'input',
-        name: 'name',
-        message: 'What is the name of your project?',
-        validate: nameInput => nameInput ? true : "Please enter your project's name!",
-      },
-      {
-        type: 'input',
-        name: 'description',
-        message: 'Provide a description of the project (Required)',
-        validate: descriptionInput => descriptionInput ? true : 'Please provide a project description!',
-      },
-      {
-        type: 'checkbox',
-        name: 'languages',
-        message: 'What did you build this project with? (Check all that apply)',
-        choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node'], 
-      },
-      {
-        type: 'input',
-        name: 'link',
-        message: 'Enter the GitHub link to your project. (Required)',
-        validate: linkInput => linkInput ? true : "You need to enter a GitHub link for your project!",
-      },
-      {
-        type: 'confirm',
-        name: 'feature',
-        message: 'Would you like to feature this project?',
-        default: true,
-      },
-      {
-        type: 'confirm',
-        name: 'confirmAddProject',
-        message: 'Would you like to enter another project?',
-        default: false,
-      }
-    ];
-
-    while (true) {
-      // projectData collects inquirer prompt data from projectQuestions.
-      const projectData = await inquirer.prompt(projectQuestions);
-      portfolioData.projects.push(projectData);
-
-      // If the user doesn't want to add another project, exit the loop.
-      if (!projectData.confirmAddProject) {
-        break;
-      }
+  // Project Name, Descrpition, Structural Languages, Link, Feature
+  const projectQuestions = [
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is the name of your project?',
+      validate: nameInput => nameInput ? true : "Please enter your project's name!",
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Provide a description of the project (Required)',
+      validate: descriptionInput => descriptionInput ? true : 'Please provide a project description!',
+    },
+    {
+      type: 'checkbox',
+      name: 'languages',
+      message: 'What did you build this project with? (Check all that apply)',
+      choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node'], 
+    },
+    {
+      type: 'input',
+      name: 'link',
+      message: 'Enter the GitHub link to your project. (Required)',
+      validate: linkInput => linkInput ? true : "You need to enter a GitHub link for your project!",
+    },
+    {
+      type: 'confirm',
+      name: 'feature',
+      message: 'Would you like to feature this project?',
+      default: true,
+    },
+    {
+      type: 'confirm',
+      name: 'confirmAddProject',
+      message: 'Would you like to enter another project?',
+      default: false,
     }
+  ];
 
-    return portfolioData;
-    
-    // .then(projectData => {
-    //   portfolioData.projects.push(projectData);
-    //   if (projectData.confirmAddProject) {
-    //     return promptProject(portfolioData);
-    //   } else {
-    //     return portfolioData;
-    //   }
-    // });
+  while (true) {
+    // projectData collects inquirer prompt data from projectQuestions.
+    const projectData = await inquirer.prompt(projectQuestions);
+    portfolioData.projects.push(projectData);
+
+    // If the user doesn't want to add another project, exit the loop.
+    if (!projectData.confirmAddProject) {
+      break;
+    }
+  }
+
+  return portfolioData;
 };
