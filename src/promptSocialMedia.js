5236687e777362f3d5d3b3a2e0ff18c4d88b31a5
Jonathan Wilferd <jwilferd10@yahoo.com>
2023-09-26 16:45:48 -0700

    promptSocialMedia.js refactored to move away from promises, utilizing async/await.

--- src/promptSocialMedia.js
+++ src/promptSocialMedia.js
@@ -2,20 +2,16 @@
 import inquirer from 'inquirer';
 
 // Function to prompt users if they'd like to include social media links
-export const promptSocialMedia = portfolioData => {
+export const promptSocialMedia = async (portfolioData) => {
   console.log(`
     ==================
     Social Media Links
     ==================
   `);
 
-  if (!portfolioData.header) {
-    // Initialize header object if it doesn't exist
-    portfolioData.header = {};
+  if (!portfolioData.socialMediaLinks) {
+    portfolioData.socialMediaLinks = [];
   }
-
-  // Array to store social media links
-  const socialMediaLinks = [];
 
   // Array of Social Media Platforms
   const socialMediaPlatforms = [
@@ -35,36 +31,62 @@ export const promptSocialMedia = portfolioData => {
       name: 'Facebook',
       key: 'faceBookLink',
     }
-  ]
+  ];
   
-  // Prompt user for each social media link
-  return inquirer.prompt(socialMediaPlatforms.map(platform => ({
-    type: 'confirm',
-    name: platform.key,
-    message: `Would you like to include a ${platform.name} link?`,
-    default: true,
-  })))
-  .then(async answers => {
-    // Prompt for usernames and store them in the socialMediaLinks array. Start with For Each
-    for (const platform of socialMediaPlatforms) {
-      if (answers[platform.key]) {
-        const usernameAnswer = await inquirer.prompt({
-          type: 'input',
-          name: platform.key,
-          message: `Enter your ${platform.name} username`,
+  // Function to prompt for specific social media platforms
+  const promptSocialMediaPlatform = async (platform) => {
+    const answer = await inquirer.prompt({
+      type: 'confirm',
+      name: platform.key,
+      message: `Would you like to include a ${platform.name} link?`,
+      default: true,
+    });
+
+    if (answer[platform.key]) {
+      const usernameAnswer = await inquirer.prompt({
+        type: 'input',
+        name: platform.key,
+        message: `Enter your ${platform.name} username`,
+      });
+
+      if (usernameAnswer[platform.key]) {
+        portfolioData.socialMediaLinks.push({
+          platform: platform.name,
+          username: usernameAnswer[platform.key],
         });
-        if (usernameAnswer[platform.key]) {
-          socialMediaLinks.push({
-            platform: platform.name,
-            username: usernameAnswer[platform.key],
-          });
-        }
       }
     }
+  };
 
-    // Store the social media links in portfolioData 
-    portfolioData.socialMediaLinks = socialMediaLinks;
+  // Prompt user for each social media link
+  for (const platform of socialMediaPlatforms) {
+    await promptSocialMediaPlatform(platform);
+  }
 
-    return portfolioData;
-  });
+  return portfolioData;
+
+
+  // .then(async answers => {
+  //   // Prompt for usernames and store them in the socialMediaLinks array. Start with For Each
+  //   for (const platform of socialMediaPlatforms) {
+  //     if (answers[platform.key]) {
+  //       const usernameAnswer = await inquirer.prompt({
+  //         type: 'input',
+  //         name: platform.key,
+  //         message: `Enter your ${platform.name} username`,
+  //       });
+  //       if (usernameAnswer[platform.key]) {
+  //         socialMediaLinks.push({
+  //           platform: platform.name,
+  //           username: usernameAnswer[platform.key],
+  //         });
+  //       }
+  //     }
+  //   }
+
+  //   // Store the social media links in portfolioData 
+  //   portfolioData.socialMediaLinks = socialMediaLinks;
+
+  //   return portfolioData;
+  // });
 };
