b3d3b672d9a38dc99066a89d7edf9c5f21751ee5
Jonathan Wilferd <jwilferd10@yahoo.com>
2023-09-26 17:06:05 -0700

    promptEducation has been refactored, consolidated the validate into one line and also added capability of adding more achievements

--- src/promptEducation.js
+++ src/promptEducation.js
@@ -2,40 +2,73 @@
 import inquirer from 'inquirer';
 
 // Function to prompt user about Education and Certification achievements
-export const promptEducation = portfolioData => {
-    if (!portfolioData.education) {
-      portfolioData.education = [];
-    }
+export const promptEducation = async (portfolioData) => {
+  if (!portfolioData.education) {
+    portfolioData.education = [];
+  }
   
-      console.log(`
-      ==============================================
-      Enter Education and Certification Achievements
-      ==============================================
-    `);
+  console.log(`
+    ==============================================
+    Enter Education and Certification Achievements
+    ==============================================
+  `);
   
-    // Name of Education and Certification, Description
-    return inquirer.prompt([
+  // Name of Education and Certification, Description
+  while (true) {
+    const educationData = await inquirer.prompt([
       {
         type: 'input',
         name: 'achievementName',
         message: 'What name can you provide? (Required)',
-        validate: achievementNameInput => {
-          if (achievementNameInput) {
-            return true;
-          } else {
-            console.log('Please enter a name or title!');
-            return false;
-          }
-        }
+        validate: achievementNameInput => achievementNameInput ? true : 'Please enter a name or title!',
       },
       {
         type: 'input',
         name: 'achievementDescription',
-        message: 'Provide a description of your achievements (Required)'
-      }, 
-    ]).then((educationData) => {
-      portfolioData.education.push(educationData);
-      // Return the updated portfolioData
-      return portfolioData;
+        message: 'Provide a description of your achievements (Required)',
+        validate: achievementDescriptionInput => achievementDescriptionInput ? true : 'Please provide a description!',
+      }
+    ]);
+
+    portfolioData.education.push(educationData);
+
+    // Ask the user if they'd like to add another detail
+    const addMore = await inquirer.prompt({
+      type: 'confirm',
+      name: 'addMore',
+      message: 'Do you want to add another noteworthy achievement?',
+      default: false,
     });
+
+    if (!addMore.addMore) {
+      break;
+    }
+  }
+
+  return portfolioData;
+
+    // return inquirer.prompt([
+    //   {
+    //     type: 'input',
+    //     name: 'achievementName',
+    //     message: 'What name can you provide? (Required)',
+    //     validate: achievementNameInput => {
+    //       if (achievementNameInput) {
+    //         return true;
+    //       } else {
+    //         console.log('Please enter a name or title!');
+    //         return false;
+    //       }
+    //     }
+    //   },
+    //   {
+    //     type: 'input',
+    //     name: 'achievementDescription',
+    //     message: 'Provide a description of your achievements (Required)'
+    //   }, 
+    // ]).then((educationData) => {
+    //   portfolioData.education.push(educationData);
+    //   // Return the updated portfolioData
+    //   return portfolioData;
+    // });
 };
