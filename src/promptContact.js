b1f2e293e9af0494860244dd73e284c85dde3c21
Jonathan Wilferd <jwilferd10@yahoo.com>
2024-11-10 22:23:52 -0800

    src/promptContact.js: Filled out the promptContact file, this file collects user input and will then display it into page-template.js.

--- src/promptContact.js
+++ src/promptContact.js
@@ -1,4 +1,51 @@
-// Collect a provided email address
-// Create a section dedicated for the Contact Section
-// Present the collected email address to the page
-// Remember to add a font from font-awesome for visual changes
+import inquirer from 'inquirer';
+
+// Prompt users to input some form of contact detail for this section
+export const promptContact = async (portfolioData) => {
+    // set as an empty array if portfolioData.about is false
+    if (!portfolioData.contact) {
+        portfolioData.contact = [];
+    }
+
+    console.log(`
+    =================================
+    Include Some Contact Information!
+    =================================
+  `);
+
+    console.log(`
+        End off with a Contact Me page! Usually included is an email or a phone-number.
+        -------------------------------------------------------------------------------
+    `);
+
+    // While true, keep running the same round of questions
+    while (true) {
+        const contactData = await inquirer.prompt([
+            {
+                type: 'input',
+                name: 'contactInfo',
+                message: 'Provide some contact details',
+                validate: contactInput => contactInput ? true : 'Please provide some details for this section!',
+            }
+        ]);
+
+        portfolioData.contact.push(contactData);
+
+        console.log(contactData);
+        
+        // Ask if users want to add another paragraph
+        const addMoreContent = await inquirer.prompt({
+            type: 'confirm',
+            name: 'addMoreContent',
+            message: 'Would you like to provide additional information?',
+            default: false,
+        });
+
+        // Exit out when not true
+        if (!addMoreContent.addMoreContent) {
+            break;
+        }
+    }
+
+    return portfolioData;
+};
