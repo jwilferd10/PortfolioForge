a8b618f81ba77f72c9df4d25e31c64d2b3e43128
Jonathan Wilferd <jwilferd10@yahoo.com>
2024-11-17 19:02:10 -0800

    src/: New directory titled template-utils has been created, this will contain all logic found within page-template.js currently. generateAbout is the first file to be created, more will follow.

---
+++ src/template-utils/generateAbout.js
@@ -0,0 +1,27 @@
+// User About Section
+const generateAbout = (aboutTextArr, emojis) => {
+    if (!aboutTextArr || aboutTextArr.length === 0) {
+      return '';
+    }
+  
+    const emojiType = emojis ? emojiMapping.comment : '';
+    const emojiHeader = generateEmojiHeader(emojiType);
+  
+    const aboutMeText = aboutTextArr.map(({ aboutText }) => {
+      return `
+        <p>${aboutText}</p>
+      `;
+    }).join(' ');
+  
+    return `
+      <section class="my-3" id="about">
+        <h2 class="text-dark bg-primary p-2 display-inline-block rounded-edges1 box-shadow1 px-3 mb-3">
+          ${emojiHeader} About Me
+        </h2>
+        <section class="my-3 aboutText">
+          ${aboutMeText}
+        </section>
+      </section>
+    `;
+};
+  
