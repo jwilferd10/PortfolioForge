4ecc70eb60655597c43facd218efea65c5fde64b
Jonathan Wilferd <jwilferd10@yahoo.com>
2024-11-17 19:23:26 -0800

    src/: generateSkillsPage now has it's own page, the logic found within page-template.js has been commented out.

--- src/page-template.js
+++ src/page-template.js
@@ -50,30 +50,30 @@ const colorThemeMapping = {
 //   `;
 // };
 
-// User Skills Section
-const generateSkillsPage = (skillsTextArr, emojis) => {
-  if (!skillsTextArr || skillsTextArr.length === 0) {
-    return '';
-  }
+// // User Skills Section
+// const generateSkillsPage = (skillsTextArr, emojis) => {
+//   if (!skillsTextArr || skillsTextArr.length === 0) {
+//     return '';
+//   }
 
-  const emojiType = emojis ? emojiMapping.tools : '';
-  const emojiHeader = generateEmojiHeader(emojiType);
+//   const emojiType = emojis ? emojiMapping.tools : '';
+//   const emojiHeader = generateEmojiHeader(emojiType);
 
-  const skillsContent = skillsTextArr.map(({ skillsText }) => {
-    return `
-      <p>${skillsText}</p>
-    `;
-  }).join(' ');
+//   const skillsContent = skillsTextArr.map(({ skillsText }) => {
+//     return `
+//       <p>${skillsText}</p>
+//     `;
+//   }).join(' ');
 
-  return `
-    <section class="my-3" id="skills">
-      <h2 class="text-dark bg-primary p-2 display-inline-block rounded-edges1 box-shadow1 px-3 mb-3">
-        ${emojiHeader} Skills
-      </h2>
-      <p>${skillsContent}</p>
-    </section>
-  `;
-};
+//   return `
+//     <section class="my-3" id="skills">
+//       <h2 class="text-dark bg-primary p-2 display-inline-block rounded-edges1 box-shadow1 px-3 mb-3">
+//         ${emojiHeader} Skills
+//       </h2>
+//       <p>${skillsContent}</p>
+//     </section>
+//   `;
+// };
 
 // Programming Languages
 const generateLanguageSpread = (languageSpread, emojis) => {
