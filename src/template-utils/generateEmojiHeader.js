6ae22b822106a5045772b3a94f2df72c04281fc5
Jonathan Wilferd <jwilferd10@yahoo.com>
2024-11-17 19:18:44 -0800

    src/template-utils/generateEmojiHeader.js: Added an example comment explaining how the function should work.

--- src/template-utils/generateEmojiHeader.js
+++ src/template-utils/generateEmojiHeader.js
@@ -1,4 +1,9 @@
 // Utility function to generate an HTML string for an Emoji Header.
+
+// Example Usage:
+// const emojiHeader = generateEmojiHeader('<i class="fas fa-comment-dots"></i>');
+// Output: '<i class="fas fa-comment-dots"></i>'
+
 export const generateEmojiHeader = emojiHeader => {
     // Ensures that a valid HTML snippet is returned if the input is non-empty
     // If the input is empty, it handles the case by returning an empty string.
