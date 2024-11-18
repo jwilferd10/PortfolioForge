6f9035cc39c311ed3d42361e9cde7076d003ea31
Jonathan Wilferd <jwilferd10@yahoo.com>
2024-11-18 07:28:51 -0800

    src/page-template: generateProjects logic has been removed, now being imported into this file.

--- src/page-template.js
+++ src/page-template.js
@@ -5,6 +5,7 @@ import { generateAbout } from './template-utils/generateAbout.js';
 import { generateSkillsPage } from './template-utils/generateSkillsPage.js';
 import { generateLanguageSpread } from './template-utils/generateLanguageSpread.js';
 import { generateAchievements } from './template-utils/generateAchievements.js';
+import { generateProjects } from './template-utils/generateProjects.js';
 
 // Mapping object for colorTheme
 const colorThemeMapping = {
@@ -18,73 +19,6 @@ const colorThemeMapping = {
   "Blue & Silver": "blue-silver.css",
   "Soft Pastel Delight": "soft-pastel.css",
   "Muted Earth Tones": "earth-tones.css"
-};
-
-// Generate the HTML containing project data
-const generateProjects = (projectsArr, emojis) => {
-
-  const emojiType = emojis ? emojiMapping.briefcase : '';
-  const emojiHeader = generateEmojiHeader(emojiType);
-
-  // In order: Featured and Non-Featured card templates
-  return `
-    <article class="my-3" id="portfolio">
-      <h2 class="text-dark bg-primary p-2 display-inline-block rounded-edges1 box-shadow1 px-3 mb-3">
-        ${emojiHeader} Work
-      </h2>
-      <section class="flex-row justify-space-between">
-        ${projectsArr.filter(({ feature }) => feature).map(({ name, description, languages, link }) => {
-          return `
-            <article class="col-12 mb-2 bg-dark text-light p-3 rounded-edges2 box-shadow2 target1">
-              <header class="text-center">
-                <h3 class="portfolio-item-title text-light">${name}</h3>
-                <h4 class="portfolio-languages">
-                  Built With:
-                  ${languages.join(', ')}
-                </h4>
-              </header>
-
-              <section>
-                <p>${description}</p>
-              </section>
-
-              <footer class="text-center"
-                <a href="${link}" class="btn">
-                  <i class="fab fa-github mr-2"></i>
-                  View Project on GitHub
-                </a>
-              </footer>
-            </article>
-          `;
-        }).join(' ')}
-
-        ${projectsArr.filter(({ feature }) => !feature).map(({ name, description, languages, link }) => { 
-          return `
-            <article class="col-12 col-md-6 mb-2 bg-dark text-light p-3 flex-column rounded-edges2 box-shadow2">
-              <header>
-                <h3 class="portfolio-item-title text-light">${name}</h3>
-                <h4 class="portfolio-languages">
-                  Built With:
-                  ${languages.join(', ')}
-                </h4>
-              </header>
-
-              <section>
-                <p>${description}</p>
-              </section>
-
-              <footer>
-                <a href="${link}" class="btn mt-auto">
-                  <i class="fab fa-github mr-2"></i>
-                  View Project on GitHub
-                </a>
-              </footer>
-            </article>
-          `;
-        }).join('')}
-      </section>
-    </article>
-  `;
 };
 
 // Select style.css based off of user choice
