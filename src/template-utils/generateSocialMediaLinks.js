a54032512cf74ac71fb7b09394853bc037be6359
Jonathan Wilferd <jwilferd10@yahoo.com>
2024-11-28 21:45:04 -0800

    src/template-utils/generateSocialMediaLinks.js: Removed redundant classes from the anchor tags.

--- src/template-utils/generateSocialMediaLinks.js
+++ src/template-utils/generateSocialMediaLinks.js
@@ -12,7 +12,7 @@ export const generataSocialMediaLinksHTML = (socialMediaData) => {
       const url = `https://${platform}.com/${username}`;
       
       return `
-        <a class="socialLinks ml-2 my-1 px-2 py-1 bg-secondary text-dark d-flex align-items-center" href="${url}">
+        <a class="ml-2 px-2 text-dark" href="${url}">
           <i class="socialIcon fab fa-${platform} fa-2x"></i>
         </a>
       `;
